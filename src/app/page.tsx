44200c7f1cd82f45602b53cb8a38b36850a05100
Aashin A K <aashin9605@gmail.com>
2024-06-19 16:12:10 +0530

    complete instructor section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import TestimonialCards from "@/components/TestimonialCards";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import UpComingWebinars from "@/components/UpComingWebinars";
+import Instructors from "@/components/Instructors";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChooseUs />
       <TestimonialCards />
       <UpComingWebinars />
+      <Instructors/>
     </main>
   );
 }
